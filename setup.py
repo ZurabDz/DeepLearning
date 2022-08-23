42071f333901d2685d8cee84485a34be2f848779
ZurabDz <zuradzindzibadze06@gmail.com>
2022-08-23 16:58:04 +0400

    simplified setup py(enough for now)

--- setup.py
+++ setup.py
@@ -1,28 +1,13 @@
-"""A setuptools based setup module.
-
-See:
-https://packaging.python.org/guides/distributing-packages-using-setuptools/
-https://github.com/pypa/sampleproject
-"""
-
 from setuptools import setup, find_packages
 
 
 setup(
-    name="deep",  # Required
-    version="2.0.0",  # Required
-    description="Deep Learning implementation",
-    url="https://github.com/pypa/sampleproject",
-    author="Zurab Dzindzibadze",
-    author_email="dzindzibadzezurabi@gmail.com",
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3 :: Only",
-    ],
-    keywords="sample, setuptools, development",
-    package_dir={"": "."},
-    packages=find_packages('.'),
-    python_requires=">=3.7",
+   name='deep_learning',
+   version='0.0.1',
+   description='Deep Learning model implementations',
+   author='Zurab Dzindzibadze',
+   author_email='dzindzibadzezurabi@gmail.com',
+   packages=find_packages(
+      exclude=['examples', 'examples.*', 'scripts', 'scripts.*', 'test', 'test.*'], 
+   ),
 )
