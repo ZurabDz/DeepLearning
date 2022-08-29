4360100b091ac55d1d0d861bcd9fdca4e47844fa
ZurabDz <zuradzindzibadze06@gmail.com>
2022-08-29 17:34:58 +0400

    small typos, docs..

--- deep_learning/utils/trainer.py
+++ deep_learning/utils/trainer.py
@@ -1,30 +1,32 @@
+"""
+Non concise way to implement Trainer like hugging face or pytorch lightning
+"""
 import torch
 
-
 class Trainer:
-    def __init__(self, max_epochs, use_gpu=0, gradient_clip_val=0) -> None:
+    def __init__(self, max_epochs, use_gpu=False, gradient_clip_val=0) -> None:
         self.max_epochs = max_epochs
         if use_gpu:
             assert torch.cuda.is_available(), 'GPU is not present'
         self.use_gpu = use_gpu
         self.gradient_clip_val = gradient_clip_val
 
-    def prepare_data(self, data):
+    def _prepare_data(self, data):
         self.train_dataloader = data.train_dataloader()
         self.val_dataloader = data.val_dataloader()
         self.num_train_batches = len(self.train_dataloader)
         self.num_val_batches = (len(self.val_dataloader) if self.val_dataloader else 0)
 
-    def prepare_model(self, model):
+    def _prepare_model(self, model):
         model.trainer = self
-        model.board.xlim = [0, self.max_epochs]
+        # model.board.xlim = [0, self.max_epochs]
         if self.use_gpu:
             model.to('cuda')
         self.model = model
 
     def fit(self, model, data):
-        self.prepare_data(data)
-        self.prepare_model(model)
+        self._prepare_data(data)
+        self._prepare_model(model)
         self.optim = model.configure_optimizers()
         self.epoch = 0
         self.train_batch_idx = 0
@@ -32,8 +34,8 @@ class Trainer:
         for self.epoch in range(self.max_epochs):
             self.fit_epoch()
 
-    def prepare_batch(self, batch):
-        if self.gpus:
+    def _prepare_batch(self, batch):
+        if self.use_gpu:
             batch = [batch.to('cuda')]
 
         return batch
@@ -49,7 +51,7 @@ class Trainer:
         self.model.train()
 
         for batch in self.train_dataloader:
-            loss = self.model.training_step(self.prepare_batch)
+            loss = self.model.training_step(self._prepare_batch(batch))
             self.optim.zero_grad()
             with torch.no_grad():
                 loss.backward()
@@ -61,7 +63,7 @@ class Trainer:
 
         if not self.val_dataloader:
             with torch.no_grad():
-                self.model.validation_step(self.prepare_batch(batch))
+                self.model.validation_step(self._prepare_batch(batch))
 
             self.val_batch_idx += 1
     
