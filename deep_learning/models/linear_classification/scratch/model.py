a38c7c26f8ca18c2ede7da7c8f5a39585e0207de
ZurabDz <zuradzindzibadze06@gmail.com>
2022-09-26 22:37:11 +0400

    working prototype

--- deep_learning/models/linear_classification/scratch/model.py
+++ deep_learning/models/linear_classification/scratch/model.py
@@ -13,6 +13,7 @@ def cross_entropy(y_hat, y):
 class SoftmaxRegression(ExtendedModule):
     def __init__(self, num_inputs, num_outpus, lr, sigma=0.1):
         super().__init__()
+        self.lr = lr
         self.w = torch.normal(0, sigma, size=(num_inputs, num_outpus), requires_grad=True)
         self.b = torch.zeros(num_outpus, requires_grad=True)
 
@@ -22,6 +23,31 @@ class SoftmaxRegression(ExtendedModule):
     def forward(self, x):
         return softmax(torch.matmul(x.reshape((-1, self.w.shape[0])), self.w) + self.b)
 
-
     def loss(self, y_hat, y):
         return cross_entropy(y_hat, y)
+
+    def configure_optimizers(self):
+        return SGD([self.w, self.b], self.lr)
+
+    def training_step(self, batch):
+        l = self.loss(self(*batch[:-1]), batch[-1])
+        self.plot('loss', l, train=True)
+        return l
+
+    def validation_step(self, batch):
+        l = self.loss(self(*batch[:-1]), batch[-1])
+
+
+class SGD:
+    def __init__(self, params, lr) -> None:
+        self.params = params
+        self.lr = lr
+
+    def step(self):
+        for param in self.params:
+            param -= self.lr * param.grad
+
+    def zero_grad(self):
+        for param in self.params:
+            if param.grad is not None:
+                param.grad.zero_()
