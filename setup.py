ea721215a59adec40101af147ade17488a8235d7
ZurabDz <zuradzindzibadze06@gmail.com>
2023-01-01 19:42:37 +0400

    removing redundent excludes

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 
 setup(
    name='deep_learning',
-   version='0.0.1',
+   version='0.0.2',
    description='Deep Learning model implementations',
    author='Zurab Dzindzibadze',
    author_email='dzindzibadzezurabi@gmail.com',
    packages=find_packages(
-      exclude=['examples', 'examples.*', 'scripts', 'scripts.*', 'test', 'test.*'], 
+      exclude=['examples', 'scripts'], 
    ),
 )
