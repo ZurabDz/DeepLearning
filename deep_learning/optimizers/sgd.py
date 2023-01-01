0bd7d513c84b3682cc97c567fb04c0b3ceb74348
ZurabDz <zuradzindzibadze06@gmail.com>
2023-01-01 20:13:14 +0400

    standalone optimizers

---
+++ deep_learning/optimizers/sgd.py
@@ -0,0 +1,13 @@
+class SGD:
+    def __init__(self, params, lr) -> None:
+        self.params = params
+        self.lr = lr
+
+    def step(self):
+        for param in self.params:
+            param -= self.lr * param.grad
+
+    def zero_grad(self):
+        for param in self.params:
+            if param.grad is not None:
+                param.grad.zero_()
